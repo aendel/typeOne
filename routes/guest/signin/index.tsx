e297386c5cafd3d4793a5a7e1b0a4a916dcde415
Francesco Vattiato <fvattiato@movia.co>
2021-03-07 03:13:23 +0100

    feat(guest-routes): Bootstraping guest routes

--- routes/guest/signin/index.tsx
+++ routes/guest/signin/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {Button, Text, View} from 'react-native';
+import {View} from 'react-native';
 import auth from '@react-native-firebase/auth';
+import {useNavigation} from '@react-navigation/native';
+import {GuestRouteName} from '../../types';
+import {TextInput, Button} from 'react-native-paper';
 
 const SignInScreen = () => {
   const {t} = useTranslation();
-
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const onAnonymousButtonPress = async () => {
     auth()
       .signInAnonymously()
@@ -20,10 +25,46 @@ const SignInScreen = () => {
         console.error(error);
       });
   };
+  const signInEmail = () => {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch((error) => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
 
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
   return (
     <View>
-      <Button title="Anonymous" onPress={onAnonymousButtonPress} />
+      <Button onPress={onAnonymousButtonPress}>Anonymous</Button>
+      <TextInput
+        label="Email"
+        placeholder="email"
+        onChangeText={setEmail}
+        value={email}
+      />
+      <TextInput
+        label="Password"
+        placeholder="password"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry={true}
+      />
+      <Button onPress={signInEmail} mode="contained">
+        {t('signin')}
+      </Button>
+      <Button onPress={() => navigation.navigate(GuestRouteName.SIGN_UP)}>
+        {t('signup')}
+      </Button>
     </View>
   );
 };
--- routes/guest/signup/index.tsx
+++ routes/guest/signup/index.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import auth from '@react-native-firebase/auth';
+import React, {useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
 
 const SignUpScreen = () => {
   const {t} = useTranslation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const createUser = () => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch((error) => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+
   return (
     <View>
-      <Text>{t('hello')}</Text>
+      <TextInput
+        label="Email"
+        placeholder="email"
+        onChangeText={setEmail}
+        value={email}
+      />
+      <TextInput
+        label="Password"
+        placeholder="password"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry={true}
+      />
+      <Button onPress={createUser}>{t('signup')}</Button>
     </View>
   );
 };
